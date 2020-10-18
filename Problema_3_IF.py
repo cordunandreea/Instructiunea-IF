54e82d7c849b8d75e0215d27d6b16cc6d63bb48b
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-10-18 18:16:28 +0300

    Problema_3_IF

---
+++ Problema_3_IF.py
@@ -0,0 +1,6 @@
+#Să se verifice dacă o literă introdusă este vocală sau consoană. Exemplu : Date de intrare a Date de ieşire vocala.
+l=str(input('litera='))
+if l=='a' or l=='e' or l=='i' or l=='o' or l=='u' :
+ print('vocala')
+else:
+    print('consoana')
